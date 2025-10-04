12b243adfc9b8a4ce897d6f2a5426b569cc3e200
Revanth2835 <revanthkumar2835@gmail.com>
2025-10-04 23:29:03 +0530

    File uploaded succesfully

---
+++ test.sql
@@ -0,0 +1,4 @@
+select * from city where countrycode = "IND" and population > 1000000;
+select name , population from city where countrycode = "IND" and population > 1000000;
+
+-- completed revising the concepts of retriving all rows , retriving only particular attributes using where clause and limit ...
